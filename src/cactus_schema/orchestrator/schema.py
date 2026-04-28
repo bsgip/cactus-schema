0e830a4c57741a604b4e3929596b63da96670011
Mike Turner <mike.turner@anu.edu.au>
2026-04-28 11:04:50 +0800

    Adds ComplianceRequestContentResponse

--- src/cactus_schema/orchestrator/schema.py
+++ src/cactus_schema/orchestrator/schema.py
@@ -220,3 +220,19 @@ class ComplianceRequestResponse(FastAPICompatibleWizard):
     user_details: str
     request_details: str
     status: str
+
+
+@dataclass
+class ComplianceRequestContentResponse(FastAPICompatibleWizard):
+    compliance_standard: str
+    witness_test: datetime
+    classes: list[str]
+    runs: list[int]
+    der_brand: str
+    der_oem: str
+    der_series: str
+    der_repmodels: str
+    software_type: str
+    software_providers: str
+    software_versions: str
+    hardware_details: str
