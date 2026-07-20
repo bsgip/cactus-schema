8e8398b4759ae30e796f4444bc0ed60f26e69a64
Joshua Vote <jjvote@gmail.com>
2026-07-20 15:24:01 +1000

    Added CSIPAusWellKnown uri (#34)

--- src/cactus_schema/runner/uri.py
+++ src/cactus_schema/runner/uri.py
@@ -6,3 +6,6 @@ Health = "/health"
 Request = "/request/{request_id}"
 RequestList = "/requests"
 Proceed = "/proceed"
+
+# This should be hosted at the root path
+CSIPAusWellKnown = "/.well-known/csipaus"
